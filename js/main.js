9bf11933b67956cb565fd85c7d3575d8d6f9f431
Mustafijur Rahman Nebir <nebir1993@gmail.com>
2016-04-09 16:29:08 +0600

    mailchimp configured
    working contact form deleted

--- js/main.js
+++ js/main.js
@@ -102,8 +102,51 @@ $(document).ready(function() {
 });
 
 
+
+/*---------------   Social Button tooltip ---------------*/
+
+
 $(document).ready(function() {
 	$(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
 	})
+});
+
+
+
+/*------------------------------------------
+ Subscribe form ajax
+ ------------------------------------------*/
+
+
+$('#subscription-form').submit(function(e) {
+
+    e.preventDefault();
+    var $form           = $('#subscription-form');
+    var submit          = $('#subscribe-button');
+    var ajaxResponse    = $('#subscription-response');
+    var email           = $('#subscriber-email').val();
+
+    $.ajax({
+        type: 'POST',
+        url: 'subscribe.php',
+        dataType: 'json',
+        data: {
+            email: email
+        },
+        cache: false,
+        beforeSend: function(result) {
+            submit.val("Joining...");
+        },
+        success: function(result) {
+            if(result.sendstatus == 1) {
+                ajaxResponse.html(result.message);
+                $form.fadeOut(500);
+            } else {
+                ajaxResponse.html(result.message);
+                submit.val("Get it!");
+            }
+        }
+    });
+
 });
